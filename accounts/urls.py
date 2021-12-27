4f35c07b96fc57d15b725a26350861059c66ffb1
Heesung Yang <fshilver@gmail.com>
2021-12-27 16:40:31 +0900

    Add profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from django.contrib.auth.views import (
     LoginView,
 )
+from django.views.generic.base import (
+    TemplateView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
 ]
